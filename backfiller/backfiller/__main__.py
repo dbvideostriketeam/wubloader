1fcd9b5b36af55e6061c21cb2b1d8167086630c0
Christopher Usher <chrusher@gmail.com>
2019-01-01 19:52:27 -0800

    Adding in stuff to hopefully get this to run

---
+++ backfiller/backfiller/__main__.py
@@ -0,0 +1,14 @@
+
+import gevent.monkey
+gevent.monkey.patch_all()
+
+import logging
+
+import argh
+
+from backfiller.main import main
+
+LOG_FORMAT = "[%(asctime)s] %(levelname)8s %(name)s(%(module)s:%(lineno)d): %(message)s"
+
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+argh.dispatch_command(main)
