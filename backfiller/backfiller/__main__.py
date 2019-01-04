db1b4e653964940a9b81e2fef462356a945696ac
Christopher Usher <chrusher@gmail.com>
2019-01-03 21:02:48 -0800

    Updated logging to match the other components

--- backfiller/backfiller/__main__.py
+++ backfiller/backfiller/__main__.py
@@ -3,6 +3,7 @@ import gevent.monkey
 gevent.monkey.patch_all()
 
 import logging
+import os
 
 import argh
 
@@ -11,4 +12,6 @@ from backfiller.main import main
 LOG_FORMAT = "[%(asctime)s] %(levelname)8s %(name)s(%(module)s:%(lineno)d): %(message)s"
 
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+level = os.environ.get('WUBLOADER_LOG_LEVEL', 'INFO').upper()
+logging.basicConfig(level=level, format=LOG_FORMAT)
 argh.dispatch_command(main)
