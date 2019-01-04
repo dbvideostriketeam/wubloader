7abff26c5de42fc16bc02f66256e49cc3a51c572
Christopher Usher <chrusher@gmail.com>
2019-01-03 21:25:43 -0800

    Fixes and suggestions from ekimekim

--- backfiller/backfiller/__main__.py
+++ backfiller/backfiller/__main__.py
@@ -11,7 +11,6 @@ from backfiller.main import main
 
 LOG_FORMAT = "[%(asctime)s] %(levelname)8s %(name)s(%(module)s:%(lineno)d): %(message)s"
 
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 level = os.environ.get('WUBLOADER_LOG_LEVEL', 'INFO').upper()
 logging.basicConfig(level=level, format=LOG_FORMAT)
 argh.dispatch_command(main)
