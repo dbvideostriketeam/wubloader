84b611055bf3f982c81d4a0fc0a9f8676f3d2e83
Mike Lang <mikelang3000@gmail.com>
2019-01-07 00:11:13 -0800

    common: convert from module to package

--- common/setup.py
+++ common/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name = "wubloader-common",
 	version = "0.0.0",
-	py_modules = ["common"],
+	packages = find_packages(),
 	install_requires = [
 		"monotonic",
 		"prometheus-client",
