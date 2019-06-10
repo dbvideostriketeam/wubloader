fdd245a6d934283d8c1d18394d6dc1a21af3d290
Mike Lang <mikelang3000@gmail.com>
2019-06-10 04:01:17 -0700

    cutter: Add lightweight youtube client
    Provides basic youtube api calls, and gets passed into both transcode checker and cutter.
    
    The official youtube client library is many orders of magnitude larger and more complicated,
    and can't actually do what we want (stream an upload of unknown size).

--- cutter/cutter/main.py
+++ cutter/cutter/main.py
@@ -1,4 +1,5 @@
 
+import json
 import logging
 import signal
 
@@ -6,10 +7,17 @@ import gevent.backdoor
 import gevent.event
 import prometheus_client as prom
 
+import common
+
+from .youtube import Youtube
+
 
 class Cutter(object):
-	def __init__(self, stop):
-		"""Stop is an Event triggering graceful shutdown when set."""
+	def __init__(self, youtube, stop):
+		"""youtube is an authenticated and initialized youtube api client.
+		Stop is an Event triggering graceful shutdown when set.
+		"""
+		self.youtube = youtube
 		self.stop = stop
 		self.logger = logging.getLogger(type(self).__name__)
 
@@ -20,10 +28,12 @@ class Cutter(object):
 
 
 class TranscodeChecker(object):
-	def __init__(self, stop):
+	def __init__(self, youtube, stop):
 		"""
+		youtube is an authenticated and initialized youtube api client.
 		Stop is an Event triggering graceful shutdown when set.
 		"""
+		self.youtube = youtube
 		self.stop = stop
 		self.logger = logging.getLogger(type(self).__name__)
 
@@ -32,7 +42,10 @@ class TranscodeChecker(object):
 			pass
 
 
-def main(metrics_port=8003, backdoor_port=0):
+def main(youtube_creds_file, metrics_port=8003, backdoor_port=0):
+	"""
+	youtube_creds_file should be a json file containing keys 'client_id', 'client_secret' and 'refresh_token'.
+	"""
 	common.PromLogCountsHandler.install()
 	common.install_stacksampler()
 	prom.start_http_server(metrics_port)
@@ -48,8 +61,14 @@ def main(metrics_port=8003, backdoor_port=0):
 	# We have two independent jobs to do - to perform cut jobs (cutter),
 	# and to check the status of transcoding videos to see if they're done (transcode checker).
 	# We want to error if either errors, and shut down if either exits.
-	cutter = Cutter(stop)
-	transcode_checker = TranscodeChecker(stop)
+	youtube_creds = json.load(open(youtube_creds_file))
+	youtube = Youtube(
+		client_id=youtube_creds['client_id'],
+		client_secret=youtube_creds['client_secret'],
+		refresh_token=youtube_creds['refresh_token'],
+	)
+	cutter = Cutter(youtube, stop)
+	transcode_checker = TranscodeChecker(youtube, stop)
 	jobs = [
 		gevent.spawn(cutter.run),
 		gevent.spawn(transcode_checker.run),
