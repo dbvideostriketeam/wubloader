2b4d2cce904b4f16a77a49492b6bb3813e06374e
Mike Lang <mikelang3000@gmail.com>
2019-06-17 02:04:22 -0700

    sheet sync: Basic skeleton

---
+++ sheetsync/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "wubloader-cutter",
+	version = "0.0.0",
+	packages = find_packages(),
+	install_requires = [
+		"argh",
+		"gevent",
+		"prometheus-client",
+		"psycogreen",
+		"psycopg2",
+		"wubloader-common",
+	],
+)
---
+++ sheetsync/sheetsync/main.py
@@ -0,0 +1,40 @@
+
+import json
+import logging
+import signal
+
+import argh
+import gevent.backdoor
+import gevent.event
+import prometheus_client as prom
+
+import common
+from common.database import DBManager
+
+
+@argh.arg('worksheet-names', nargs='+', help="The names of the individual worksheets within the sheet to operate on.")
+def main(dbconnect, sheets_creds_file, sheet_id, worksheet_names, metrics_port=8004, backdoor_port=0):
+	"""dbconnect should be a postgres connection string, which is either a space-separated
+	list of key=value pairs, or a URI like:
+		postgresql://USER:PASSWORD@HOST/DBNAME?KEY=VALUE
+
+	sheets_creds_file should be a json file containing keys 'client_id', 'client_secret' and 'refresh_token'.
+	"""
+	common.PromLogCountsHandler.install()
+	common.install_stacksampler()
+	prom.start_http_server(metrics_port)
+
+	if backdoor_port:
+		gevent.backdoor.BackdoorServer(('127.0.0.1', backdoor_port), locals=locals()).start()
+
+	stop = gevent.event.Event()
+	gevent.signal(signal.SIGTERM, stop.set) # shut down on sigterm
+
+	logging.info("Starting up")
+
+	dbmanager = DBManager(dsn=dbconnect)
+	sheets_creds = json.load(open(sheets_creds_file))
+
+	# TODO the thing
+
+	logging.info("Gracefully stopped")
