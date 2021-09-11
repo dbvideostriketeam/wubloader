d5b5b012bff03b0e3374219fdc3ec8c1a02e3ac6
Mike Lang <mikelang3000@gmail.com>
2021-09-11 13:17:31 +1000

    wip:

--- cut_sync_race.py
+++ cut_sync_race.py
@@ -80,7 +80,7 @@ def add_range(base, range):
 
 @argh.arg('--time-offset', type=float)
 def main(match_id, race_number, output_path,
-	host='condor.live', user='necrobot-read', password='necrobot-read', database='condor_x2',
+	host='condor.live', user='necrobot-read', password='necrobot-read', database='condor_xii',
 	base_dir='/srv/wubloader',
 	start_range="0,10", non_interactive=False, racer=0,
 	time_offset=None,
