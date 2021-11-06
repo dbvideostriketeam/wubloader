978ea2bb51e153835ae4f75a404f5f0f334d274f
ElementalAlchemist <ElementAlchemist7@gmail.com>
2021-11-06 00:31:56 -0500

    Initial transition from VideoJS to HLS.js as the video player

--- thrimbletrimmer/scripts/keyboard-shortcuts.js
+++ thrimbletrimmer/scripts/keyboard-shortcuts.js
@@ -1,5 +1,6 @@
-function moveSpeed(player, amount) {
-	let currentIndex = PLAYBACK_RATES.indexOf(player.playbackRate());
+function moveSpeed(amount) {
+	const videoElement = document.getElementById("video");
+	let currentIndex = PLAYBACK_RATES.indexOf(videoElement.playbackRate);
 	if (currentIndex === -1) {
 		addError("The playback rate has somehow gone very wrong.");
 		return;
@@ -8,15 +9,15 @@ function moveSpeed(player, amount) {
 	if (currentIndex < 0 || currentIndex >= PLAYBACK_RATES.length) {
 		return; // We've reached/exceeded the edge
 	}
-	player.playbackRate(PLAYBACK_RATES[currentIndex]);
+	videoElement.playbackRate = PLAYBACK_RATES[currentIndex];
 }
 
-function increaseSpeed(player) {
-	moveSpeed(player, 1);
+function increaseSpeed() {
+	moveSpeed(1);
 }
 
-function decreaseSpeed(player) {
-	moveSpeed(player, -1);
+function decreaseSpeed() {
+	moveSpeed(-1);
 }
 
 document.addEventListener("keypress", (event) => {
@@ -24,69 +25,69 @@ document.addEventListener("keypress", (event) => {
 		return;
 	}
 
-	const player = getVideoJS();
+	const videoElement = document.getElementById("video");
 	switch (event.key) {
 		case "0":
-			player.currentTime(0);
+			videoElement.currentTime = 0;
 			break;
 		case "1":
-			player.currentTime(player.duration() * 0.1);
+			videoElement.currentTime = videoElement.duration * 0.1;
 			break;
 		case "2":
-			player.currentTime(player.duration() * 0.2);
+			videoElement.currentTime = videoElement.duration * 0.2;
 			break;
 		case "3":
-			player.currentTime(player.duration() * 0.3);
+			videoElement.currentTime = videoElement.duration * 0.3;
 			break;
 		case "4":
-			player.currentTime(player.duration() * 0.4);
+			videoElement.currentTime = videoElement.duration * 0.4;
 			break;
 		case "5":
-			player.currentTime(player.duration() * 0.5);
+			videoElement.currentTime = videoElement.duration * 0.5;
 			break;
 		case "6":
-			player.currentTime(player.duration() * 0.6);
+			videoElement.currentTime = videoElement.duration * 0.6;
 			break;
 		case "7":
-			player.currentTime(player.duration() * 0.7);
+			videoElement.currentTime = videoElement.duration * 0.7;
 			break;
 		case "8":
-			player.currentTime(player.duration() * 0.8);
+			videoElement.currentTime = videoElement.duration * 0.8;
 			break;
 		case "9":
-			player.currentTime(player.duration() * 0.9);
+			videoElement.currentTime = videoElement.duration * 0.9;
 			break;
 		case "j":
-			player.currentTime(player.currentTime() - 10);
+			videoElement.currentTime -= 10;
 			break;
 		case "k":
 		case " ":
-			if (player.paused()) {
-				player.play();
+			if (videoElement.paused) {
+				videoElement.play();
 			} else {
-				player.pause();
+				videoElement.pause();
 			}
 			break;
 		case "l":
-			player.currentTime(player.currentTime() + 10);
+			videoElement.currentTime += 10;
 			break;
 		case "ArrowLeft":
-			player.currentTime(player.currentTime() - 5);
+			videoElement.currentTime -= 5;
 			break;
 		case "ArrowRight":
-			player.currentTime(player.currentTime() + 5);
+			videoElement.currentTime += 5;
 			break;
 		case ",":
-			player.currentTime(player.currentTime() - 1 / VIDEO_FRAMES_PER_SECOND);
+			videoElement.currentTime -= 1 / VIDEO_FRAMES_PER_SECOND;
 			break;
 		case ".":
-			player.currentTime(player.currentTime() + 1 / VIDEO_FRAMES_PER_SECOND);
+			videoElement.currentTime += 1 / VIDEO_FRAMES_PER_SECOND;
 			break;
 		case "=":
-			increaseSpeed(player);
+			increaseSpeed();
 			break;
 		case "-":
-			decreaseSpeed(player);
+			decreaseSpeed();
 			break;
 		case "[":
 			if (typeof setCurrentRangeStartToVideoTime === "function") {
@@ -119,13 +120,13 @@ document.addEventListener("keydown", (event) => {
 		return;
 	}
 
-	const player = getVideoJS();
+	const videoElement = document.getElementById("video");
 	switch (event.key) {
 		case "ArrowLeft":
-			player.currentTime(player.currentTime() - 5);
+			videoElement.currentTime -= 5;
 			break;
 		case "ArrowRight":
-			player.currentTime(player.currentTime() + 5);
+			videoElement.currentTime += 5;
 			break;
 		default:
 			break;
