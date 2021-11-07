f7c05ff53dc4908e67904c1c91b8e019ca573d65
HeNine <>
2021-11-07 16:07:30 +0100

    less

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ bash fetch_models.sh
 docker build -f buscribe/Dockerfile -t buscribe:$VERSION .
 docker build -f buscribe-api/Dockerfile -t buscribe-api:$VERSION .
 
-docker build -f less/Dockerfile -t lessc
+docker build -f docker-less/Dockerfile -t lessc .
 docker run --rm -v "$(pwd)"/buscribe-web:/buscribe-web lessc /buscribe-web/style.less > buscribe-web/style.css
 
 docker build -f nginx/Dockerfile -t buscribe-web:$VERSION .
