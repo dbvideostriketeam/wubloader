dc4e7f08355cbebd62a13f03e69d47a8775207b4
ElementalAlchemist <ElementAlchemist7@gmail.com>
2022-10-16 20:47:30 -0500

    Implement chat transcript for editor and chat replay for restreamer page

--- thrimbletrimmer/scripts/stream.js
+++ thrimbletrimmer/scripts/stream.js
@@ -4,6 +4,7 @@ const TIME_FRAME_AGO = 3;
 
 var globalLoadedVideoPlayer = false;
 var globalVideoTimeReference = TIME_FRAME_AGO;
+var globalChatPreviousRenderTime = null;
 
 window.addEventListener("DOMContentLoaded", async (event) => {
 	commonPageSetup();
@@ -55,6 +56,11 @@ window.addEventListener("DOMContentLoaded", async (event) => {
 	});
 
 	updateTimeSettings();
+
+	await getStreamChatLog();
+	const videoPlayer = document.getElementById("video");
+	videoPlayer.addEventListener("loadedmetadata", (_event) => initialChatRender());
+	videoPlayer.addEventListener("timeupdate", (_event) => updateChatRender());
 });
 
 async function loadDefaults() {
@@ -220,5 +226,96 @@ function convertEnteredTimes() {
 		document.getElementById("time-converter-from-bus").checked = true;
 	} else if (timeConvertTo === TIME_FRAME_AGO) {
 		document.getElementById("time-converter-from-ago").checked = true;
+	}
+}
+
+async function getStreamChatLog() {
+	const startTime = getStartTime();
+	const endTime = getEndTime();
+	if (!startTime || !endTime) {
+		return;
+	}
+	return getChatLog(wubloaderTimeFromDateTime(startTime), wubloaderTimeFromDateTime(endTime));
+}
+
+function initialChatRender() {
+	if (!globalChatData) {
+		return;
+	}
+	const videoPlayer = document.getElementById("video");
+	const videoTime = videoPlayer.currentTime;
+	const videoDateTime = dateTimeFromVideoPlayerTime(videoTime);
+	const chatReplayContainer = document.getElementById("chat-replay");
+	chatReplayContainer.innerHTML = "";
+
+	for (const chatMessage of globalChatData) {
+		if (chatMessage.when > videoDateTime) {
+			break;
+		}
+		handleChatMessage(chatReplayContainer, chatMessage);
+	}
+
+	globalChatPreviousRenderTime = videoTime;
+}
+
+function updateChatRender() {
+	if (!globalChatData) {
+		return;
+	}
+	const videoPlayer = document.getElementById("video");
+	const videoTime = videoPlayer.currentTime;
+
+	if (videoTime < globalChatPreviousRenderTime) {
+		initialChatRender();
+	} else {
+		const videoDateTime = dateTimeFromVideoPlayerTime(videoTime);
+		const lastAddedTime = dateTimeFromVideoPlayerTime(globalChatPreviousRenderTime);
+		const chatReplayContainer = document.getElementById("chat-replay");
+
+		let rangeMin = 0;
+		let rangeMax = globalChatData.length;
+		let lastChatIndex = Math.floor((rangeMin + rangeMax) / 2);
+		while (rangeMax - rangeMin > 1) {
+			if (globalChatData[lastChatIndex].when === lastAddedTime) {
+				break;
+			}
+			if (globalChatData[lastChatIndex].when < lastAddedTime) {
+				rangeMin = lastChatIndex;
+			} else {
+				rangeMax = lastChatIndex;
+			}
+			lastChatIndex = Math.floor((rangeMin + rangeMax) / 2);
+		}
+
+		for (let chatIndex = lastChatIndex + 1; chatIndex < globalChatData.length; chatIndex++) {
+			const chatMessage = globalChatData[chatIndex];
+			if (chatMessage.when > videoDateTime) {
+				break;
+			}
+			handleChatMessage(chatReplayContainer, chatMessage);
+		}
+	}
+	globalChatPreviousRenderTime = videoTime;
+}
+
+function handleChatMessage(chatReplayContainer, chatMessage) {
+	if (chatMessage.message.command === "PRIVMSG") {
+		const chatDOM = renderChatMessage(chatMessage);
+		if (chatDOM) {
+			chatReplayContainer.appendChild(chatDOM);
+		}
+	} else if (chatMessage.message.command === "CLEARMSG") {
+		const removedID = chatMessage.message.tags["target-msg-id"];
+		const targetMessageElem = document.getElementById(`chat-replay-message-${removedID}`);
+		if (targetMessageElem) {
+			targetMessageElem.classList.add("chat-replay-message-cleared");
+		}
+	} else if (chatMessage.message.command === "CLEARCHAT") {
+		const removedSender = chatMessage.message.params[1];
+		for (const messageElem of chatReplayContainer.children) {
+			if (messageElem.dataset.sender === removedSender) {
+				messageElem.classList.add("chat-replay-message-cleared");
+			}
+		}
 	}
 }
