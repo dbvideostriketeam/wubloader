7c135751d3e8f8deec1a6a5b433263b10ec4d691
Christopher Usher <chrusher@gmail.com>
2023-11-13 13:15:05 -0800

    Starting integration of donation graphs

---
+++ graphs/graphs/__main__.py
@@ -0,0 +1,15 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+
+import logging
+import os
+
+import argh
+
+from graphs.main import main
+# from main import main
+
+LOG_FORMAT = "[%(asctime)s] %(levelname)8s %(name)s(%(module)s:%(lineno)d): %(message)s"
+
+logging.basicConfig(level='INFO', format=LOG_FORMAT)
+argh.dispatch_command(main)
---
+++ graphs/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'graphs',
+    version = '0.0.0',
+    packages = find_packages(),
+    install_requires = [
+            'argh',
+            'bokeh',
+            'gevent',
+            'request'
+    ],
+)
