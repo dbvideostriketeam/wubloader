2d40893825470a047f290815861bf3e45af81a63
Mike Lang <mikelang3000@gmail.com>
2024-10-18 08:59:07 +1100

    Move buscribe-web to top level

--- buscribe-web/script.js
+++ buscribe-web/script.js
@@ -27,7 +27,7 @@ function query(text, start_time, end_time) {
 
     const channel = document.getElementById("channel_select").value;
 
-    fetch(`https://wubloader.raptorpond.com/buscribe/${channel}/json?${query_string}`)
+    fetch(`/buscribe-api/${channel}/json?${query_string}`)
         .then(response => response.json())
         .then(fillResults)
 
