7fe04e2938e62b6251d115b27b5fbf09292031d0
Christopher Usher <chrusher@gmail.com>
2024-10-19 19:23:12 -0700

    Updating thumbnail generation to use database for templates and allow crop and location to be varied

--- common/common/images.py
+++ common/common/images.py
@@ -1,32 +1,20 @@
 
-import json
-import os
 import sys
 from io import BytesIO
 
 from PIL import Image
 
+
 """
-A template is two files:
-	NAME.png
-	NAME.json
-The image is the template image itself.
-The JSON file contains the following:
-	{
-		crop: BOX,
-		location: BOX,
-	}
-where BOX is a 4-tuple [left x, top y, right x, bottom y] describing a rectangle in image coordinates.
+A thumbnail template consists of a byte stream representation of a PNG template image along with two 4-tuples [left x, top y, right x, bottom y] describing the crop and location of the frame. 
 
 To create a thumbnail, the input frame is first cropped to the bounds of the "crop" box,
 then resized to the size of the "location" box, then pasted underneath the template image at that
 location within the template image.
 
-For example, a JSON file of:
-	{
-		"crop": [50, 100, 1870, 980],
-		"location": [320, 180, 1600, 900]
-	}
+For example
+crop = (50, 100, 1870, 980)
+location = (320, 180, 1600, 900)
 would crop the input frame from (50, 100) to (1870, 980), resize it to 720x1280,
 and place it at (320, 180).
 
@@ -34,18 +22,13 @@ If the original frame and the template differ in size, the frame is first resize
 This allows you to work with a consistent coordinate system regardless of the input frame size.
 """
 
-def compose_thumbnail_template(base_dir, template_name, frame_data):
-	template_path = os.path.join(base_dir, "thumbnail_templates", f"{template_name}.png")
-	info_path = os.path.join(base_dir, "thumbnail_templates", f"{template_name}.json")
+def compose_thumbnail_template(template_data, frame_data, crop, location):
 
-	template = Image.open(template_path)
 	# PIL can't load an image from a byte string directly, we have to pretend to be a file
+	template = Image.open(BytesIO(template_data))
 	frame = Image.open(BytesIO(frame_data))
 
-	with open(info_path) as f:
-		info = json.load(f)
-	crop = info['crop']
-	loc_left, loc_top, loc_right, loc_bottom = info['location']
+	loc_left, loc_top, loc_right, loc_bottom = location
 	location = loc_left, loc_top
 	location_size = loc_right - loc_left, loc_bottom - loc_top
 
@@ -72,14 +55,15 @@ def compose_thumbnail_template(base_dir, template_name, frame_data):
 	buf.seek(0)
 	return buf.read()
 
-
-def cli(template_name, image, base_dir="."):
-	with open(image, "rb") as f:
-		image = f.read()
-	thumbnail = compose_thumbnail_template(base_dir, template_name, image)
+def cli(template, frame, crop, location):
+	with open(template, "rb") as f:
+		template = f.read()
+	with open(frame, "rb") as f:
+		frame = f.read()
+	thumbnail = compose_thumbnail_template(template, frame, crop, location)
 	sys.stdout.buffer.write(thumbnail)
 
 
 if __name__ == '__main__':
 	import argh
-	argh.dispatch_command(cli)
+	argh.dispatch_command(cli)		
