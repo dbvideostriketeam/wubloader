715d8e5517f80fbc615b8e7e2a1149b12b650514
ElementalAlchemist <ElementAlchemist7@gmail.com>
2024-11-07 23:43:20 -0600

    Initial structure and clock
    This is the start of replacing the old Thrimbletrimmer with a new SolidJS-based Thrimbletrimmer. It includes an implementation of the clock page for a basic implementation of signal-based page.

---
+++ thrimbletrimmer/vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+
+export default defineConfig({
+	base: "/thrimbletrimmer/",
+	plugins: [devtools(), solidPlugin()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+		rollupOptions: {
+			input: {
+				index: fileURLToPath(new URL("index.html", import.meta.url)),
+				edit: fileURLToPath(new URL("edit.html", import.meta.url)),
+				utils: fileURLToPath(new URL("utils.html", import.meta.url)),
+				thumbnails: fileURLToPath(new URL("thumbnails.html", import.meta.url)),
+			},
+		},
+	},
+});
