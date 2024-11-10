c54bd3c63f502be3211feddfadbc735836f25a95
ElementalAlchemist <ElementAlchemist7@gmail.com>
2024-11-10 00:19:59 -0600

    Add errors and keyboard shortcuts to restreamer

--- thrimbletrimmer/src/restreamer/Restreamer.tsx
+++ thrimbletrimmer/src/restreamer/Restreamer.tsx
@@ -1,7 +1,27 @@
-import { Component } from "solid-js";
+import { Accessor, Component, createSignal, For } from "solid-js";
+import styles from "./Restreamer.module.scss";
+import { KeyboardShortcuts } from "../common/videoKeyboardShortcuts";
 
 const Restreamer: Component = () => {
-	return <></>;
+	const [pageErrors, setPageErrors] = createSignal<string[]>([]);
+
+	return (
+		<>
+			<ul class={styles.errorList}>
+				<For each={pageErrors()}>
+					{(error: string, index: Accessor<number>) => (
+						<li>
+							{error}
+							<a class={styles.errorRemoveLink}>[X]</a>
+						</li>
+					)}
+				</For>
+			</ul>
+			<div class={styles.keyboardShortcutHelp}>
+				<KeyboardShortcuts includeEditorShortcuts={false} />
+			</div>
+		</>
+	);
 };
 
 export default Restreamer;
