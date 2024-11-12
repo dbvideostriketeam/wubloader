df722fbe7a2d5347467de1dc17e63cf453adb25b
ElementalAlchemist <ElementAlchemist7@gmail.com>
2024-11-12 03:34:38 -0600

    Implement basic video controls
    These will be expanded or replaced with vidstack's controls in the future, but this at least gives us most of the basic requirements.

--- thrimbletrimmer/src/common/video.tsx
+++ thrimbletrimmer/src/common/video.tsx
@@ -1,4 +1,13 @@
-import { Accessor, Component, createEffect, createSignal, Setter, Show } from "solid-js";
+import {
+	Accessor,
+	Component,
+	createEffect,
+	createSignal,
+	For,
+	onCleanup,
+	Setter,
+	Show,
+} from "solid-js";
 import { DateTime } from "luxon";
 import {
 	TimeType,
@@ -10,6 +19,14 @@ import {
 	dateTimeFromTimeAgo,
 } from "./convertTime";
 import styles from "./video.module.scss";
+import { MediaPlayerElement } from "vidstack/elements";
+import { VideoQuality } from "vidstack";
+
+import playImage from "../images/video-controls/play.png";
+import pauseImage from "../images/video-controls/pause.png";
+import volumeImage from "../images/video-controls/volume.png";
+import volumeMuteImage from "../images/video-controls/volume-mute.png";
+import fullscreenImage from "../images/video-controls/fullscreen.png";
 
 export const VIDEO_FRAMES_PER_SECOND = 30;
 
@@ -176,6 +193,161 @@ export const StreamTimeSettings: Component<StreamTimeSettingsProps> = (props) =>
 				</div>
 			</Show>
 		</form>
+	);
+};
+
+export interface VideoControlsProps {
+	mediaPlayer: Accessor<MediaPlayerElement>;
+}
+
+export const VideoControls: Component<VideoControlsProps> = (props) => {
+	const mediaPlayer = props.mediaPlayer();
+	if (!mediaPlayer) {
+		return <></>;
+	}
+
+	const [isPlaying, setIsPlaying] = createSignal(!props.mediaPlayer().paused);
+	const [playerTime, setPlayerTime] = createSignal(props.mediaPlayer().currentTime);
+	const [duration, setDuration] = createSignal(props.mediaPlayer().duration);
+	const [isMuted, setIsMuted] = createSignal(props.mediaPlayer().muted);
+	const [volume, setVolume] = createSignal(props.mediaPlayer().volume);
+	const [playbackRate, setPlaybackRate] = createSignal(props.mediaPlayer().playbackRate);
+	const [qualityLevel, setQualityLevel] = createSignal<VideoQuality | null>(
+		props.mediaPlayer().state.quality,
+	);
+	const [qualityLevelList, setQualityLevelList] = createSignal(props.mediaPlayer().state.qualities);
+	const [isFullscreen, setIsFullscreen] = createSignal(false);
+
+	const unsubscribe = props.mediaPlayer().subscribe((playerState) => {
+		setIsPlaying(!playerState.paused);
+		setPlayerTime(playerState.currentTime);
+		setDuration(playerState.duration);
+		setIsMuted(playerState.muted);
+		setVolume(playerState.volume);
+		setPlaybackRate(playerState.playbackRate);
+		setQualityLevel(playerState.quality);
+		setQualityLevelList(playerState.qualities);
+		setIsFullscreen(playerState.fullscreen);
+
+		if (playerState.fullscreen) {
+			props.mediaPlayer().controls.show();
+		} else {
+			props.mediaPlayer().controls.hide();
+		}
+	});
+
+	createEffect(() => {
+		const player = props.mediaPlayer();
+		if (isFullscreen() && !player.controls.showing) {
+			player.controls.show();
+		} else if (!isFullscreen() && player.controls.showing) {
+			player.controls.hide();
+		}
+	});
+
+	onCleanup(() => unsubscribe());
+
+	const timeDisplay = (time: number) => {
+		const hours = Math.floor(time / 3600);
+		const minutes = Math.floor((time / 60) % 60);
+		const seconds = Math.floor((time % 60) * 1000) / 1000;
+
+		const minutesDisplay = minutes < 10 ? `0${minutes}` : minutes.toString();
+		const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds.toString();
+
+		if (hours === 0) {
+			return `${minutesDisplay}:${secondsDisplay}`;
+		}
+		return `${hours}:${minutesDisplay}:${secondsDisplay}`;
+	};
+
+	const playerTimeDisplay = () => timeDisplay(playerTime());
+
+	const durationDisplay = () => timeDisplay(duration());
+
+	return (
+		<div class={styles.videoControls}>
+			<div class={styles.videoControlsBar}>
+				<div>
+					<img
+						src={isPlaying() ? pauseImage : playImage}
+						alt={isPlaying() ? "pause" : "play"}
+						class="click"
+						onClick={(event) => (props.mediaPlayer().paused = !props.mediaPlayer().paused)}
+					/>
+				</div>
+				<div>
+					{playerTimeDisplay()} / {durationDisplay()}
+				</div>
+				<div class={styles.videoControlsSpacer}></div>
+				<div class={styles.videoControlsVolume}>
+					<img
+						src={isMuted() ? volumeMuteImage : volumeImage}
+						alt={isMuted() ? "muted" : "volume"}
+						class="click"
+						onClick={(event) => (props.mediaPlayer().muted = !props.mediaPlayer().muted)}
+					/>
+					<progress
+						value={volume()}
+						class={`click ${styles.videoControlsVolumeLevel}`}
+						onClick={(event) => {
+							const player = props.mediaPlayer();
+							player.volume = event.offsetX / event.currentTarget.offsetWidth;
+						}}
+					/>
+				</div>
+				<div>
+					<select
+						value={playbackRate()}
+						onSelect={(event) => (props.mediaPlayer().playbackRate = +event.currentTarget.value)}
+					>
+						<For each={PLAYBACK_RATES}>
+							{(item, index) => <option value={item}>{item}x</option>}
+						</For>
+					</select>
+				</div>
+				<div>
+					<select
+						value={qualityLevel() ? qualityLevel().id : ""}
+						onSelect={(event) =>
+							(props.mediaPlayer().qualities[event.currentTarget.selectedIndex].selected = true)
+						}
+					>
+						<For each={qualityLevelList()}>
+							{(item, index) => <option value={index()}>{item.id}</option>}
+						</For>
+					</select>
+				</div>
+				<div>
+					<img
+						src={fullscreenImage}
+						alt="fullscreen"
+						class="click"
+						onClick={(event) => {
+							const player = props.mediaPlayer();
+							if (!player.state.canFullscreen) {
+								return;
+							}
+							if (isFullscreen()) {
+								player.exitFullscreen();
+							} else {
+								player.requestFullscreen();
+							}
+						}}
+					/>
+				</div>
+			</div>
+			<progress
+				class={`click ${styles.videoControlsPlaybackPosition}`}
+				value={duration() === 0 ? 0 : (playerTime() / duration())}
+				onClick={(event) => {
+					const player = props.mediaPlayer();
+					const progressProportion = event.offsetX / event.currentTarget.offsetWidth;
+					const time = progressProportion * duration();
+					player.currentTime = time;
+				}}
+			/>
+		</div>
 	);
 };
 
