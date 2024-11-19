c1e0d8490fdf5ea966a58ce8be99096bbfdb732d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2024-11-19 01:57:45 -0600

    Respect the URL parameters when loading the restreamer

--- thrimbletrimmer/src/common/streamInfo.tsx
+++ thrimbletrimmer/src/common/streamInfo.tsx
@@ -4,4 +4,34 @@ export class StreamVideoInfo {
 	streamName: string;
 	streamStartTime: DateTime;
 	streamEndTime: DateTime | null;
+
+	public static defaultFromURL(): StreamVideoInfo | null {
+		const url = new URL(window.location.href);
+		const urlParams = url.searchParams;
+		const stream = urlParams.get("stream");
+		const start = urlParams.get("start");
+		const end = urlParams.get("end");
+		if (stream === null) {
+			return null;
+		}
+		if (start === null) {
+			return null;
+		}
+		const startTime = DateTime.fromISO(start, { zone: "utc" });
+		if (!startTime.isValid) {
+			return null;
+		}
+		let endTime: DateTime | null = null;
+		if (end !== null) {
+			endTime = DateTime.fromISO(end, { zone: "utc" });
+			if (!endTime.isValid) {
+				return null;
+			}
+		}
+		return {
+			streamName: stream,
+			streamStartTime: startTime,
+			streamEndTime: endTime,
+		};
+	}
 }
