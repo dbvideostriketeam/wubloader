f9589a101f1fa5f55dbcc4cf13ef3759b8b427a9
Mike Lang <mikelang3000@gmail.com>
2025-10-25 19:00:57 +1100

    media downloads: add accept header
    Fixes #502

--- common/common/media.py
+++ common/common/media.py
@@ -23,6 +23,9 @@ from .stats import timed
 # Lots of things will tell you to go away if you don't look like a browser
 # (eg. imgur)
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
+# Other things will serve you html unless you specifically ask for an image (eg. reddit).
+# We also take this opportunity to ask for a higher-quality or lossless version.
+ACCEPT = "image/avif,image/webp,image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5"
 
 
 media_bytes_downloaded = prom.Counter(
@@ -272,6 +275,7 @@ def _request(url, max_size, content_types):
 
 	headers = {
 		"User-Agent": USER_AGENT,
+		"Accept": ACCEPT,
 	}
 	conn.request("GET", url, headers=headers, preload_content=False)
 	resp = conn.getresponse()
