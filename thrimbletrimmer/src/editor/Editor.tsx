c0d39b7a3f112cd461a6cbb5354457ab728317a3
ElementalAlchemist <ElementAlchemist7@gmail.com>
2025-11-16 18:51:05 -0600

    Implement the waveform view in the editor

--- thrimbletrimmer/src/editor/Editor.tsx
+++ thrimbletrimmer/src/editor/Editor.tsx
@@ -1,6 +1,7 @@
 import {
 	Accessor,
 	Component,
+	createEffect,
 	createResource,
 	createSignal,
 	Index,
@@ -9,8 +10,8 @@ import {
 	Show,
 	Suspense,
 } from "solid-js";
+import { leadingAndTrailing, throttle } from "@solid-primitives/scheduled";
 import { DateTime } from "luxon";
-import { Fragment } from "hls.js";
 import { MediaPlayerElement } from "vidstack/elements";
 import styles from "./Editor.module.scss";
 import { StreamVideoInfo } from "../common/streamInfo";
@@ -321,6 +322,12 @@ const EditorContent: Component<ContentProps> = (props) => {
 				videoFragments={videoFragmentTimes}
 				videoDuration={videoDuration}
 			/>
+			<Waveform
+				videoInfo={streamVideoInfo}
+				videoQuality={props.data.video_quality}
+				videoTime={videoPlayerTime}
+				videoDuration={videoDuration}
+			/>
 		</>
 	);
 };
@@ -372,3 +379,54 @@ function videoPlayerTimeFromDateTime(
 	}
 	return null;
 }
+
+interface WaveformProperties {
+	videoInfo: Accessor<StreamVideoInfo>;
+	videoQuality: string;
+	videoTime: Accessor<number>;
+	videoDuration: Accessor<number>;
+}
+
+const Waveform: Component<WaveformProperties> = (props) => {
+	const generateWaveformURL = () => {
+		const videoInfo = props.videoInfo();
+		const videoQuality = props.videoQuality;
+		const videoDuration = props.videoDuration();
+
+		const start = wubloaderTimeFromDateTime(videoInfo.streamStartTime);
+		const end = videoInfo.streamEndTime ? wubloaderTimeFromDateTime(videoInfo.streamEndTime) : null;
+
+		const query = new URLSearchParams({
+			size: "1920x125",
+			d: videoDuration.toString(),
+			color: "#e5e5e5",
+			start: start,
+		});
+		if (end !== null) {
+			query.append("end", end);
+		}
+
+		return `/waveform/${videoInfo.streamName}/${videoQuality}.png?${query.toString()}`;
+	};
+
+	const [waveformURL, setWaveformURL] = createSignal(generateWaveformURL());
+
+	const setWaveformURLTrigger = leadingAndTrailing(throttle, setWaveformURL, 15000);
+
+	createEffect(() => {
+		const waveformURL = generateWaveformURL();
+		setWaveformURLTrigger(waveformURL);
+	});
+
+	const markerStyle = () => {
+		const videoPercentage = (props.videoTime() * 100) / props.videoDuration();
+		return `left: ${videoPercentage}%`;
+	};
+
+	return (
+		<div class={styles.waveformContainer}>
+			<img class={styles.waveform} alt="Waveform for this video" src={waveformURL()} />
+			<div class={styles.waveformMarker} style={markerStyle()}></div>
+		</div>
+	);
+};
