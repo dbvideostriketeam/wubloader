805a4092b3b7405eac1cf56d2e3dded4b1c1af1b
ElementalAlchemist <ElementAlchemist7@gmail.com>
2025-11-23 03:06:52 -0600

    Implement image cropping for thumbnails

--- thrimbletrimmer/src/common/binding.tsx
+++ thrimbletrimmer/src/common/binding.tsx
@@ -33,12 +33,67 @@ export function bindingTextareaOnChange(
 	createEffect(() => (element.value = s()));
 }
 
+export function bindingInputNumberOnChange(
+	element: HTMLInputElement,
+	accessor: () => [Accessor<number>, Setter<number>],
+) {
+	const [s, set] = accessor();
+	element.addEventListener("change", (event) =>
+		set(+(event.currentTarget as HTMLInputElement).value),
+	);
+	createEffect(() => (element.value = s().toString()));
+}
+
+export function bindingInputPositiveNumberOnChange(
+	element: HTMLInputElement,
+	accessor: () => [Accessor<number>, Setter<number>],
+) {
+	const [s, set] = accessor();
+	element.addEventListener("change", (event) => {
+		const elementValue = +(event.currentTarget as HTMLInputElement).value;
+		if (elementValue > 0) {
+			set(elementValue);
+		}
+	});
+	createEffect(() => (element.value = s().toString()));
+}
+
+export function bindingInputPositiveNumberOrZeroOnChange(
+	element: HTMLInputElement,
+	accessor: () => [Accessor<number>, Setter<number>],
+) {
+	const [s, set] = accessor();
+	element.addEventListener("change", (event) => {
+		const elementValue = +(event.currentTarget as HTMLInputElement).value;
+		if (elementValue >= 0) {
+			set(elementValue);
+		}
+	});
+	createEffect(() => (element.value = s().toString()));
+}
+
+export function bindingInputChecked(
+	element: HTMLInputElement,
+	accessor: () => [Accessor<boolean>, Setter<boolean>],
+) {
+	const [s, set] = accessor();
+	element.addEventListener("change", (event) => {
+		const elementChecked = (event.currentTarget as HTMLInputElement).checked;
+		set(elementChecked);
+	});
+	createEffect(() => (element.checked = s()));
+}
+
 declare module "solid-js" {
 	namespace JSX {
 		interface DirectiveFunctions {
 			bindingInputOnChange: typeof bindingInputOnChange;
 			bindingInputOnInput: typeof bindingInputOnInput;
 			bindingTextareaOnChange: typeof bindingTextareaOnChange;
+			bindingInputNumberOnChange: typeof bindingInputNumberOnChange;
+			bindingInputPositiveNumberOnChange: typeof bindingInputPositiveNumberOnChange;
+			bindingInputPositiveNumberOrZeroOnChange: typeof bindingInputPositiveNumberOrZeroOnChange;
+			bindingInputChecked: typeof bindingInputChecked;
 		}
 	}
 }
---
+++ thrimbletrimmer/src/common/thumbnails.tsx
@@ -0,0 +1,2 @@
+export const BASE64_PNG_PREFIX = "data:image/png;base64,";
+export const BASE64_PNG_PREFIX_LENGTH = 22;
