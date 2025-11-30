1839cf76fd581ac93bc54216a55a8007aaad1d19
ElementalAlchemist <ElementAlchemist7@gmail.com>
2025-11-30 01:03:14 -0600

    Allow replacing the utilities page title with the clock

--- thrimbletrimmer/src/utilities/Clock.tsx
+++ thrimbletrimmer/src/utilities/Clock.tsx
@@ -1,13 +1,17 @@
-import { Accessor, Component, createSignal, onCleanup, onMount } from "solid-js";
+import { Accessor, Component, createMemo, createSignal, onCleanup, onMount } from "solid-js";
+import { useTitle } from "solidjs-use";
 import { DateTime, Interval } from "luxon";
+import { bindingInputChecked } from "../common/binding";
 
 interface ClockProps {
 	busStartTime: Accessor<DateTime | null>;
+	initialPageTitle: string;
 }
 
 const Clock: Component<ClockProps> = (props) => {
-	const [delay, setDelay] = createSignal<number>(10);
-	const [time, setTime] = createSignal<DateTime>(DateTime.utc());
+	const [delay, setDelay] = createSignal(10);
+	const [time, setTime] = createSignal(DateTime.utc());
+	const [useTimeAsTitle, setUseTimeAsTitle] = createSignal(false);
 	const busStartTime = props.busStartTime;
 
 	const timer = setInterval(() => setTime(DateTime.utc()), 250);
@@ -32,6 +36,15 @@ const Clock: Component<ClockProps> = (props) => {
 		return `${sign}${timeElapsedString}`;
 	};
 
+	const titleToUse = createMemo(() => {
+		const time = timeDisplay();
+		if (useTimeAsTitle()) {
+			return time;
+		}
+		return props.initialPageTitle;
+	});
+	useTitle(titleToUse);
+
 	return (
 		<div>
 			<div>{timeDisplay()}</div>
@@ -44,6 +57,12 @@ const Clock: Component<ClockProps> = (props) => {
 					onInput={(event) => setDelay(+event.currentTarget.value)}
 				/>
 				&#32;seconds of delay
+			</div>
+			<div>
+				<label>
+					<input type="checkbox" use:bindingInputChecked={[useTimeAsTitle, setUseTimeAsTitle]} />
+					&#32;Show time in page title
+				</label>
 			</div>
 		</div>
 	);
