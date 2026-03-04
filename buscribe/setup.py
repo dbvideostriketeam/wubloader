658e6f825be4dba831a113746187aeeabdb35bc0
HubbeKing <github@hubbe.club>
2026-03-04 22:09:27 +0200

    Upgrade buscribe to debian 13 and fix EXTERNALLY-MANAGED warning

--- buscribe/setup.py
+++ buscribe/setup.py
@@ -7,10 +7,8 @@ setup(
     install_requires = [
         "argh",
         "psycopg2",
-        #"gevent==1.5a2",
-	"gevent",
-        #"greenlet==0.4.16",
-	"greenlet",
+	    "gevent",
+	    "greenlet",
         "psycogreen",
         "wubloader-common",
         "python-dateutil",
