2489b73dabf700ee51617815d7adf385649a912b
HubbeKing <github@hubbe.club>
2026-03-12 10:22:24 +0200

    Make sheetsync use debian:13

--- sheetsync/setup.py
+++ sheetsync/setup.py
@@ -5,10 +5,9 @@ setup(
 	version = "0.0.0",
 	packages = find_packages(),
 	install_requires = [
-		"argh==0.28.1",
+		"argh==0.29.4",
 		"gevent",
-		"exceptiongroup", # backport from python 3.11
-		"prometheus-client==0.7.1", # locked version as we rely on internals
+		"prometheus-client",
 		"psycogreen",
 		"psycopg2",
 		"python-dateutil",
